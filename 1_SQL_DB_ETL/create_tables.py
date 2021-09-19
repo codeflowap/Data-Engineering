a4decbe1adfe383560e396d2903cdbe2a3fa7913
Ahmad Pour <dram62@gmail.com>
2021-09-19 04:41:12 +0300

    modified db name to studentdb

--- 1_SQL_DB_ETL/create_tables.py
+++ 1_SQL_DB_ETL/create_tables.py
@@ -1,3 +1,13 @@
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
+def create_database():
+    """
+    - Creates and connects to the sparkifydb
+    - Returns the connection and cursor to sparkifydb
+    """
+
+    # connect to default database
+    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=postgres password=admin")
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
