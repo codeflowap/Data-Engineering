a614950d68f31596407de2494a639f22f3cf52c3
Ahmad Pour <dram62@gmail.com>
2021-09-19 03:17:36 +0300

    drop table queries added

--- 1_SQL_DB_ETL/sql_queries.py
+++ 1_SQL_DB_ETL/sql_queries.py
@@ -1,10 +1,10 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE songplay"
+user_table_drop = "DROP TABLE user"
+song_table_drop = "DROP TABLE song"
+artist_table_drop = "DROP TABLE artist"
+time_table_drop = "DROP TABLE time"
 
 # CREATE TABLES
 
