e7ab2261d28769a979a07e9a567ac7f74dc85520
Ahmad Pour <dram62@gmail.com>
2021-09-19 03:25:38 +0300

    create table queries added

--- 1_SQL_DB_ETL/sql_queries.py
+++ 1_SQL_DB_ETL/sql_queries.py
@@ -12,16 +12,16 @@ songplay_table_create = (""" CREATE TABLE IF NOT EXISTS songplays (songplay_id t
                             level text, song_id text, artist_id text, session_id int, location text, user_agent text)
                             """)
 
-user_table_create = (""" CREATE TABLE IF NOT EXISTS user_id int, first_name text, last_name text, gender text, level 
-                        text """)
+user_table_create = (""" CREATE TABLE IF NOT EXISTS user (user_id int, first_name text, last_name text, gender text, level 
+                        text) """)
 
-song_table_create = (""" song_id text, title text, artist_id text, year int, duration float8
+song_table_create = (""" CREATE TABLE IF NOT EXISTS song (song_id text, title text, artist_id text, year int, duration float8)
                         """)
 
-artist_table_create = (""" artist_id text, name text, location txt, latitude float8, longitude float8
+artist_table_create = (""" CREATE TABLE IF NOT EXISTS artist (artist_id text, name text, location txt, latitude float8, longitude float8)
                         """)
 
-time_table_create = (""" start_time time, hour int, day int, week int, month int, year int, weekday int
+time_table_create = (""" CREATE TABLE IF NOT EXISTS time (start_time time, hour int, day int, week int, month int, year int, weekday int)
                     """)
 
 # INSERT RECORDS
