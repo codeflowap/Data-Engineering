4672872f6284da67486b88f649dfe0700666fc45
Ahmad Pour <dram62@gmail.com>
2021-09-19 18:50:20 +0300

    created etl.py | imported necessary packages

--- 1_SQL_DB_ETL/etl.py
+++ 1_SQL_DB_ETL/etl.py
@@ -3,3 +3,9 @@ import glob
 import psycopg2
 import pandas as pd
 from sql_queries import *
+
+filepath = './data'
+for root, dirs, files in os.walk(filepath):
+    # join the file path and roots with the subdirectories using glob
+    file_path_list = glob.glob(os.path.join(root, '*'))
+    print(file_path_list)
