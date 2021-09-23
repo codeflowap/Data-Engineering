a23b972bc01d647e7a0c108cb3b1edc12e867a92
Ahmad Pour <dram62@gmail.com>
2021-09-23 10:52:20 +0300

    create a united csv from all csv in subdirectories

--- 2_NoSQL_DB_ETL/etl.py
+++ 2_NoSQL_DB_ETL/etl.py
@@ -11,20 +11,20 @@ import psycopg2
 
 import sql_queries
 
-def get_file_pathes():
+def get_file_paths(data_directory = '/event_data'):
     """ collect and join each file path in all subdirectories """
 
     # checking your current working directory
     print(os.getcwd())
 
     # Get your current folder and subfolder event data
-    filepath = os.getcwd() + '/event_data'
+    filepath = os.getcwd() + data_directory
 
     # Create a for loop to create a list of files and collect each filepath
     for root, dirs, files in os.walk(filepath):
         # join the file path and roots with the subdirectories using glob
         file_path_list = glob.glob(os.path.join(root, '*.csv'))
-    print('get_file_pathes SUCCESS')
+    print('SUCCESS: file paths collected')
     return file_path_list
 
 def get_full_data(file_path_list):
@@ -48,14 +48,47 @@ def get_full_data(file_path_list):
             for line in csvreader:
                 # print(line)
                 full_data_rows_list.append(line)
-    print(full_data_rows_list[0])
+    print('SUCCESS: data list created')
+    return full_data_rows_list
+
+def create_csv_from_list(data_list):
+
+    """ create a csv file, set the header with desired column names, and then INSERT the
+        data from a list to the csv """
+
+    csv.register_dialect('myDialect', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+
+    with open('event_datafile_new.csv', 'w', encoding='utf8', newline='') as f:
+        writer = csv.writer(f, dialect='myDialect')
+        writer.writerow(['artist', 'firstName', 'gender', 'itemInSession', 'lastName', 'length', \
+                         'level', 'location', 'sessionId', 'song', 'userId'])
+        for row in data_list:
+            if (row[0] == ''):
+                continue
+            writer.writerow((row[0], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[12], row[13], row[16]))
+    # check the number of rows in your csv file
+    with open('event_datafile_new.csv', 'r', encoding = 'utf8') as f:
+        print('SUCCESS: csv from data list created. Total lines = ', sum(1 for line in f))
+
+def process_data():
+
+    """ stores a csv into postgres database"""
+
+
+
+
 
 def main():
     conn = psycopg2.connect("host=127.0.0.1 dbname=csvtodb user=postgres password=admin")
     cur = conn.cursor()
 
-    file_path_list = get_file_pathes()
-    get_full_data(file_path_list)
+    # create a united csv from all csv in subdirectories
+    data_directory = '/event_data'
+    file_path_list = get_file_paths(data_directory='/event_data')
+    full_data_rows_list = get_full_data(file_path_list)
+    create_csv_from_list(full_data_rows_list)
+
+
 
 
     conn.close()
