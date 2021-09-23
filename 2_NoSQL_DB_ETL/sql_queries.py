b4c43c7e224c0f5ebe06182fc95eb315dcf28b8f
Ahmad Pour <dram62@gmail.com>
2021-09-23 11:25:52 +0300

    added INSERT query

--- 2_NoSQL_DB_ETL/sql_queries.py
+++ 2_NoSQL_DB_ETL/sql_queries.py
@@ -7,3 +7,7 @@ songtable_table_drop = "DROP TABLE songtable"
 
 create_table_queries = [songplay_table_create]
 drop_table_queries = [songtable_table_drop]
+
+songtable_table_insert = """ INSERT INTO songtable (artist, firstName, gender, itemInSession, lastName, length, level,
+                                location, sessionId, song, userId) 
+                                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) """
